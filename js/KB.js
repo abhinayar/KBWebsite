149f9f4a0761dfee5041c5ef758a40111887d323
Abhi Nayar <abhi_nayar@Abhis-MacBook-Air.local>
2015-06-18 18:01:42 +0530

    Built out homepage

--- js/KB.js
+++ js/KB.js
@@ -38,7 +38,7 @@
     var scroll_pos = 0;
     $(document).scroll(function() { 
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 75) {
+        if(scroll_pos > 80) {
             $("#branding").css('background-image', 'url(./img/header/logo_black.png)');
         } else {
             $("#branding").css('background-image', 'url(./img/header/logo_white.png)');
@@ -49,7 +49,44 @@
     $('.problem-carousel-js').carousel({
 		      interval: 5000
             });
-
+    
+    $('#whatWeDoCarousel').carousel({
+		      interval: 50000
+            });
+    
+    $('#whatWeDoCarousel').on('slide.bs.carousel', function(e) {
+        var from = $('.tab-style li.active').index();
+        var next = $(e.relatedTarget);
+        var to =  next.index();
+        $('#whatWeDoCarouselButtons').find('li').removeClass('active').eq(to).addClass('active');
+    });
+    
+    $("#IndiaWest").click(function(){
+        window.location = $(this).find("a:first").attr("href");
+        return false;
+    });
+    
+    $("#Forbes").click(function(){
+        window.location = $(this).find("a:first").attr("href");
+        return false;
+    });
+    
+    $("#NPR").click(function(){
+        window.location = $(this).find("a:first").attr("href");
+        return false;
+    });
+    
+    $("#YaleNews").click(function(){
+        window.location = $(this).find("a:first").attr("href");
+        return false;
+    });
+    
+    $("#CoDesign").click(function(){
+        window.location = $(this).find("a:first").attr("href");
+        return false;
+    });
+    
+  
     /*$("header").css("background-image", "url(../img/bg_new_scaled.jpg)"); */
         
 })(jQuery); // End of use strict
